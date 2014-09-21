1f601ca00a10fef6739d3bfa1ece9a61b876070e
Riza Fahmi <rizafahmi@gmail.com>
2014-09-21 09:26:54 +0700

    Add postgrex and ecto

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule PhoenixJobsFour.Mixfile do
   def application do
     [
       mod: { PhoenixJobsFour, [] },
-      applications: [:phoenix, :cowboy, :logger]
+      applications: [:phoenix, :cowboy, :logger, :postgrex, :ecto]
     ]
   end
 
@@ -25,7 +25,9 @@ defmodule PhoenixJobsFour.Mixfile do
   defp deps do
     [
       {:phoenix, "0.4.1"},
-      {:cowboy, "~> 1.0.0"}
+      {:cowboy, "~> 1.0.0"},
+      {:postgrex, "0.6.0"},
+      {:ecto, "0.2.5"}
     ]
   end
 end
