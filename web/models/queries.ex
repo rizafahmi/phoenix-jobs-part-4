6e2c1267d7037e0235080e464e159c36c286850f
Riza Fahmi <rizafahmi@gmail.com>
2014-09-21 09:36:59 +0700

    Add queries

---
+++ web/models/queries.ex
@@ -0,0 +1,20 @@
+defmodule PhoenixJobsFour.Queries do
+  import Ecto.Query
+  alias PhoenixJobsFour.Jobs
+  alias PhoenixJobsFour.Repo
+
+  def jobs_query do
+    query = from job in Jobs,
+            order_by: [desc: job.id],
+            select: job
+    Repo.all(query)
+  end
+
+  def job_detail_query(id) do
+    int_id = String.to_integer(id)
+    query = from job in Jobs,
+            where: job.id == ^int_id,
+            select: job
+    Repo.all(query) |> List.first
+  end
+end
