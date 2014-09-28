9c0e1d4b14e16722245fa223ee22caa4b4b86154
Riza Fahmi <rizafahmi@gmail.com>
2014-09-28 20:30:32 +0700

    Add new user route

--- web/router.ex
+++ web/router.ex
@@ -4,6 +4,7 @@ defmodule PhoenixJobsFour.Router do
   scope alias: PhoenixJobsFour do
     get "/", PageController, :index, as: :pages
     resources "/jobs", JobController
+    get "/users/new", UserController, :new, as: :user
   end
 
 end
