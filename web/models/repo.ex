c6d857c6ca96843d997a97db6d18fa946506ab90
Riza Fahmi <rizafahmi@gmail.com>
2014-11-27 17:29:22 +0700

    Change the database setting

--- web/models/repo.ex
+++ web/models/repo.ex
@@ -2,7 +2,7 @@ defmodule PhoenixJobsFour.Repo do
     use Ecto.Repo, adapter: Ecto.Adapters.Postgres
 
     def conf do
-      parse_url "ecto://riza:220281@localhost/phoenix_jobs_four"
+      parse_url "ecto://riza:220281@localhost/phoenix-jobs"
     end
 
     def priv do
